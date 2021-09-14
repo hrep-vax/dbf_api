4c1f27b6205fd1f987bdaef17f0673666b9217d1
Jego Carlo Ramos <jego.ramos@blackpearl.technology>
2021-09-14 12:18:44 +0800

    modified migrations schema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,19 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('middle_name')->nullable();
             $table->string('email')->unique();
             $table->string('password');
-            $table->date('birthday')->nullable();
-            $table->string('mobile_number')->nullable();
-            $table->string('sex')->nullable();
-            $table->string('profile_picture_url')->nullable();
-            $table->string('home_address')->nullable();
-            $table->string('barangay')->nullable();
-            $table->string('city')->nullable();
-            $table->string('region')->nullable();
             $table->timestamps();
         });
     }
