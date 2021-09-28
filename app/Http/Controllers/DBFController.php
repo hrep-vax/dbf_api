0f4d1db83e8b4091e3fe3846479096971ce5537d
lei-pot <81949481+lei-pot@users.noreply.github.com>
2021-09-28 14:38:49 +0800

    add endpoint
    add endpoint

--- app/Http/Controllers/DBFController.php
+++ app/Http/Controllers/DBFController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use XBase\TableReader;
+use XBase\TableEditor;
 use App\Traits\ApiResponder;
 use Exception;
 
@@ -44,7 +45,31 @@ class DBFController extends Controller
    */
   public function store(Request $request)
   {
-    //
+    // add record
+    $table = new TableEditor(
+      resource_path('dbf\CHECKS.DBF'),
+      [
+        'editMode' => TableEditor::EDIT_MODE_CLONE, //default
+      ]
+    );
+    $record = $table->appendRecord();
+    $record->set('voucher', $request->voucher);
+    $record->set('check', $request->check);
+    $record->set('code', $request->code);
+    $record->set('oblig', $request->oblig);
+    $record->set('obj_clas', $request->obj_clas);
+    $record->set('che_date', $request->che_date);
+    $record->set('date_sign', $request->date_sign);
+    $record->set('rec_date', $request->rec_date);
+    $record->set('rec_opis', $request->rec_opis);
+    $record->set('ret_date', $request->ret_date);
+    $record->set('ret_time', $request->ret_time);
+    $record->set('rel_date', $request->rel_date);
+    $record->set('rel_name', $request->rel_name);
+    $record->set('or_number', $request->or_number);
+    $record->set('amount', $request->amount);
+    $table->writeRecord();
+    return $this->success(['message' => 'Record Added Successfully'], 200);
   }
 
   /**
@@ -53,9 +78,22 @@ class DBFController extends Controller
    * @param  int  $id
    * @return \Illuminate\Http\Response
    */
-  public function show($id)
+  public function show(Request $request, $id)
   {
-    //
+    $table = new TableReader(resource_path('dbf\CHECKS.DBF'));
+
+    $emp_id = $request->emp_id;
+
+    $records = [];
+    $record_entry = [];
+    $column_headers = $table->getColumns();
+    while ($record = $table->nextRecord()) {
+      foreach ($column_headers as $column) {
+        $record_value = $record->get($column->getName());
+      }
+    }
+
+    return $this->success(['dbf' => $record_value], 200);
   }
 
   /**
@@ -67,7 +105,18 @@ class DBFController extends Controller
    */
   public function update(Request $request, $id)
   {
-    //
+    // update record
+
+    $table = new TableEditor(resource_path('dbf\CHECKS.DBF'));
+
+    for ($i = 0; $i < 10; $i++) {
+      $record = $table->nextRecord();
+      $record->set("voucher", $request->voucher);
+
+
+      $table->writeRecord();
+    }
+    return $this->success(['message' => 'Record Updated Successfully'], 200);
   }
 
   /**
--- routes/api_routes/dbf.php
+++ routes/api_routes/dbf.php
@@ -4,3 +4,9 @@ use App\Http\Controllers\DBFController;
 
 /** GET    /api/profile/me    Show current auth user's profile info    Private **/
 Route::middleware('auth:sanctum')->get('dbf-view', [DBFController::class, 'index'])->name('dbf-view');
+
+Route::middleware('auth:sanctum')->post('dbf-update', [DBFController::class, 'update'])->name('dbf-update');
+
+Route::middleware('auth:sanctum')->post('dbf-add', [DBFController::class, 'store'])->name('dbf-add');
+
+Route::middleware('auth:sanctum')->post('dbf-show', [DBFController::class, 'show'])->name('dbf-show');
