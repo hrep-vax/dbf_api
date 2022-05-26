06c0c414f7f89339984ea8071b873232e19dd58f
lei-pot <81949481+lei-pot@users.noreply.github.com>
2022-05-26 10:52:47 +0800

    upload dbf file to s3 bucket
    wala pa po itong read file sir.. hehe

--- app/Http/Controllers/DBFController.php
+++ app/Http/Controllers/DBFController.php
@@ -7,12 +7,8 @@ use XBase\TableReader;
 use XBase\TableEditor;
 use App\Traits\ApiResponder;
 use Exception;
-use App\Models\Payee2;
-use App\Models\Payee;
-use App\Models\Check;
 use App\Models\Checks_record;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Http\File;
 
 class DBFController extends Controller
 {
@@ -22,31 +18,16 @@ class DBFController extends Controller
    *
    * @return \Illuminate\Http\Response
    */
-  public function index(String $filename)
+  public function index(String $url)
   {
-    //show all DBF data
-    //$table = new TableReader(resource_path('dbf\special\ramaneta\JAN22.DBF'));
-    //$table = new TableReader(resource_path('dbf\special\rata\jan22.DBF'));
-    // $table = new TableReader(resource_path('dbf\special\rata\feb22.DBF'));
-    //$table = new TableReader(resource_path('dbf\special\rata\mar22.DBF'));
-    $table = new TableReader(storage_path('app\\' . $filename));
 
-    // $table = new TableReader(resource_path("dbf\.'$name'.DBF"));
-    //$table = new TableReader(resource_path('dbf\CHECKS.DBF')); // eto yung nagbabago lagi kailangan mag upload ng 
-    //user ng update sa dbf, so make sure na makukuha ng system un upload nila
-    // $table = new TableReader(resource_path('dbf\PAYEES.DBF'));
-    //$table = new TableReader(resource_path('dbf\med50k.DBF'));
-    // // trigger batch process : specific time
-    // // create log file  to check records count kung nag tally sa dbf, and then unique check no.
-    // // return sucess message
-    // $path = Storage::disk('public')->path($filename);
-    // $resource_path = Storage::disk($foldername)->path($filename); //check if file exist
+    // $table = new TableReader(storage_path('app\\' . $filename));
 
-    //$resource_path = Storage::path($filename);
-    // $table = new TableReader(storage_path($resource_path));
-    //$table = new TableReader(storage_path('dbf\c_advice\c_advice.DBF'));
-    // $filename = 'c_advice.dbf';
-    //$table = new TableReader(storage_path('app\\dbf\\c_advice\\c_advice.dbf'));
+    // $contents = Storage::get($url);
+    $contents = Storage::disk('s3')->get($url);
+    $test_content = Storage::disk('s3')->put($url, $contents);
+    $table = new TableReader($test_content);
+
 
     $records = [];
     $record_entry = [];
@@ -55,10 +36,8 @@ class DBFController extends Controller
       foreach ($column_headers as $column) {
         try {
 
-
           $record_value = utf8_encode($record->get($column->getName()));
 
-          //comment
           $record_entry[$column->getName()] = $record_value;
         } catch (Throwable $e) {
         }
@@ -68,10 +47,6 @@ class DBFController extends Controller
       array_push($records, $record_entry);
     }
 
-    ///////////////////////////////////////
-    //Check::insert($records);
-    //Payee::insert($records);
-    //Checks_record::insert($records);
 
     $insert_data = collect($records); // Make a collection to use the chunk method
 
@@ -145,75 +120,9 @@ class DBFController extends Controller
         array_push($records, $record_entry);
       }
     }
-    //declare another array
-    //name galing sa show2;records
-    //array push
-    //$name =  $this->show2($emp_id);
+
     $final_output =  $records;
     return $this->success(["dbf" => $final_output], 200);
-
-
-    /*$table = new TableReader(resource_path('dbf\CHECKS.DBF'));
-    //$table = new TableReader(resource_path('dbf\MEDIC2.DBF'));
-
-    $emp_id = $request->emp_id;a
-
-    $records = [];
-    $record_entry = [];
-    while ($record = $table->nextRecord()) {
-      $record_value = $record->get('code');
-      if ($emp_id == $record_value) {
-        $record_entry["voucher"] = $record->get('voucher');
-        $record_entry["check"] = $record->get('check');
-        $record_entry["code"] = $record->get('code');
-        $record_entry["oblig"] = $record->get('oblig');
-        $record_entry["obj_clas"] = $record->get('oblig');
-        $record_entry["che_date"] = $record->get('che_date');
-        $record_entry["date_sign"] = $record->get('date_sign');
-        $record_entry["rec_date"] = $record->get('rec_date');
-        $record_entry["rec_opis"] = $record->get('rec_opis');
-        $record_entry["ret_date"] = $record->get('ret_date');
-        $record_entry["ret_time"] = $record->get('ret_time');
-        $record_entry["rel_date"] = $record->get('rel_date');
-        $record_entry["rel_name"] = $record->get('rel_name');
-        $record_entry["or_number"] = $record->get('or_number');
-        $record_entry["amount"] = $record->get('amount');
-        /*$record_entry["code"] = $record->get('code');
-        $record_entry["amount"] = $record->get('amount');
-        $record_entry["orno"] = $record->get('orno');
-        $record_entry["duedate"] = $record->get('duedate');
-        $record_entry["date_used"] = $record->get('date_used');*/
-    /*array_push($records, $record_entry);
-      }
-    }
-
-    return $this->success(['dbf' => $records], 200);*/
-  }
-
-  public function show2($id)
-  {
-
-
-    $table = new TableReader(resource_path('dbf\NAME2.DBF'));
-
-    //echo ($request);3 
-
-    $records2 = [];
-    $emp_id2 = $id;
-    $record_entry2 = [];
-    while ($record2 = $table->nextRecord()) {
-      $record_value2 = $record2->get('code');
-      if ($emp_id2 == $record_value2) {
-
-        $record_entry2["name"] = $record2->get('name');
-        //$record_entry2["code"] = $record2->get('code');
-
-        array_push($records2, $record_entry2);
-      }
-    }
-    //$data = "Sir Andy";
-    $data = $records2;
-    return $data;
   }
 
   /**
@@ -254,21 +163,16 @@ class DBFController extends Controller
   {
     $file = $request->file('file');
     $filename = $file->getClientOriginalName();
-    $foldername = $request->foldername;
-
+    // $foldername = $request->foldername;
+    /** Save to S3 */
+    $t = Storage::disk('s3')->put($filename, file_get_contents($file), 'public');
+    /** get S3 url*/
+    $url = Storage::disk('s3')->url($filename);
     //$path = Storage::disk('local')->put($foldername, $file);
-    $path = Storage::disk('local')->put($filename, file_get_contents($file));
+    //$path = Storage::disk('local')->put($filename, file_get_contents($file));
 
-    // Manually specify a filename...
-    //$path = Storage::putFileAs('photos', new File('/path/to/photo'), 'photo.jpg');
-    //$path = Storage::putFileAs('dbf', new File('storage/app'), $filename);
-    // $path = Storage::putFile($file, new File('local'));
-    //$path = Storage::put($filename, $file);
-
-    // $path = $file->put($filename);
-    //$path = $file->storeAs('resources/dbf/', $filename); 
-    if ($path) {
-      self::index($filename); //send folder name and file name to convert
+    if ($url) {
+      self::index($url); //send folder name and file name to convert
       return response()->json(['message' => 'file uploaded'], 200);
     } else {
       return response()->json(['message' => 'file upload error'], 503);
